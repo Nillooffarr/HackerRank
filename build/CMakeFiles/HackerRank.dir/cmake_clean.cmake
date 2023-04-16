4ccf473791f42f033ace30956be44af0c7bdb114
Niloofar <niloofar.honarmand@gmail.com>
2023-04-16 18:32:12 +0200

    update .gitignore file

---
+++ build/CMakeFiles/HackerRank.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HackerRank.dir/main.cpp.o"
+  "CMakeFiles/HackerRank.dir/main.cpp.o.d"
+  "HackerRank"
+  "HackerRank.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HackerRank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/niloofar/coding_trials/c++/HackerRank
+# Build directory: /Users/niloofar/coding_trials/c++/HackerRank/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
