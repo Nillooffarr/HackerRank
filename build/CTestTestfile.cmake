0618c24dc000dcb2658d4e790623fce261476d58
Niloofar Honarmand <51516862+Nillooffarr@users.noreply.github.com>
2023-04-16 18:33:22 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/niloofar/coding_trials/c++/HackerRank
-# Build directory: /Users/niloofar/coding_trials/c++/HackerRank/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
